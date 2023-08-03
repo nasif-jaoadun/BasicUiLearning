1e3fb1216273a69d8aa8f56028579f1328062247
nasif-jaoadun <nasif_online@yahoo.com>
2023-08-03 13:07:42 +0600

    Position views with linear layout: Simple constraint layout

--- app/src/main/java/com/codehabit/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/codehabit/android/userinterface/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "This is the entered name: ${binding.contentView.editText.text}", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "This is the entered name:", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
 
